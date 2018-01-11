94364d3503c3f48c5d2964c80fc63b7aba4cf66d
mikestepanov <mikestepanov777@gmail.com>
2018-01-10 20:43:06 -0800

    added /channel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var items = require('../database-mysql');
+var config = require('../config.js');
+
+var token = config.token;
+console.log(token);
 
 var app = express();
 
@@ -18,4 +23,9 @@ app.get('/items', function (req, res) {
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
+});
+
+app.get('/channels', function(req, res) {
+  var base = 'https://slack.com/api/channels.list';
+  var url = `${base}?token=${token}`;
 });
