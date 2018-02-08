72174f4408ebe6d9dfcae48339e6c376ce894914
mikestepanov <mikestepanov777@gmail.com>
2018-02-07 17:28:46 -0800

    100% acceptance

--- mysql/index.js
+++ mysql/index.js
@@ -3,7 +3,6 @@ var request = require('request');
 var config = require('../config.js');
 
 var tokens88 = config.tokens88;
-var tokens87 = config.tokens87;
 var token = config.token;
 
 var getPublicChannels = function(callback) {
@@ -59,29 +58,30 @@ var memeIt = function(channel, messages, callback) {
 'christmasparrot', 'congapartyparrot', 'dealwithitparrot', 'discoparrot', 'matrixparrot', 'moonwalkingparrot', 'pirateparrot', 'sithparrot', 'thumbsupparrot', 'upvotepartyparrot', 'wendyparrot'];;
   var angela = ['angela-admirable', 'angela-adorable', 'angela-adventurous', 'angela-artistic', 'angela-affable', 'angela-agile', 'angela-all-star', 'angela-ambitious', 'angela-american-airlines', 'angela-amused', 'angela-angelic', 'angela-angular', 'angela-app', 'angela-architecture', 'angela-array', 'angela-array-is-array', 'angela-ascendant', 'angela-asha', 'angela-astonishing', 'angela-asynchronous', 'angela-avid', 'angela-awesome', 'angela-aws'];
   var names = parrots;
-
+  var time = Date.now();
   for (var tokenId of tokens88) {
     for (var message of messages) {
       var timeStamp = message.ts;
       for (var i = 0; i < names.length; i++) {
-        setTimeout(function(){
-            { meme(names[i]) }
-            console.log(names[i]);
-        }, i * 1000);
-
-        function meme(name) {
-          var base = 'https://slack.com/api/reactions.add';
-          var url = `${base}?token=${tokenId}&channel=${channel}&timestamp=${timeStamp}&name=${name}`;
-          // console.log(url);
-          request.get({
-            url: url,
-            json: true,
-            headers: {'User-Agent': 'request'}
-          }, (err, res, data) => {
-          });
-        }
+        doSetTimeout(i, names[i], timeStamp, tokenId);
       }
     }
+  }
+
+  function doSetTimeout(i, name, timeStamp, tokenId) {
+    setTimeout(function() { meme(name, timeStamp, tokenId) }, i * 1000);
+  }
+
+  function meme(name, timeStamp, tokenId) {
+    var base = 'https://slack.com/api/reactions.add';
+    var url = `${base}?token=${tokenId}&channel=${channel}&timestamp=${timeStamp}&name=${name}`;
+    console.log(url);
+    request.get({
+      url: url,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+    });
   }
   callback(null, 555);
 };
