775e8062c86b80d50d378953f67d832729df0976
Mikhail Stepanov <mikestepanov777@gmail.com>
2018-06-22 23:57:24 -0700

    can now cache & deploy

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require('express')
+
+const app = express()
+
+app.get('/func/timestamp', (req, res) => {
+  res.send(`${Date.now()}`)
+})
+
+app.get('/func/timestamp-cached', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  res.send(`${Date.now()}`)
+})
+
+exports.app = functions.https.onRequest(app)
